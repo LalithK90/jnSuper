1bf8acb0d6817e3ad00968005d7b2877db4103b0
Tharaka <tharaka-jayaweera@outlook.com>
2021-01-15 11:59:17 +0530

    final one

--- src/main/java/j_n_super_pvt_ltd/asset/branch/dao/BranchDao.java
+++
@@ -1,9 +0,0 @@
-package j_n_super_pvt_ltd.asset.branch.dao;
-
-import j_n_super_pvt_ltd.asset.branch.entity.Branch;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BranchDao extends JpaRepository<Branch, Integer> {
-}
--- src/main/java/j_n_super_pvt_ltd/asset/category/dao/CategoryDao.java
+++
@@ -1,9 +0,0 @@
-package j_n_super_pvt_ltd.asset.category.dao;
-
-import j_n_super_pvt_ltd.asset.category.entity.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CategoryDao extends JpaRepository<Category, Integer> {
-}
